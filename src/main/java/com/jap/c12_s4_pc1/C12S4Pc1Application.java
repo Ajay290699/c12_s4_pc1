78dfd9d77c24554c46256e99442e0993d4476413
Ajay Chavan <ajayc9838@gmail.com>
2022-12-22 17:36:45 +0530

    task completed

---
+++ src/main/java/com/jap/c12_s4_pc1/C12S4Pc1Application.java
@@ -0,0 +1,14 @@
+package com.jap.c12_s4_pc1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C12S4Pc1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(C12S4Pc1Application.class, args);
+		System.out.println("Good day to you");
+	}
+
+}
---
+++ src/test/java/com/jap/c12_s4_pc1/C12S4Pc1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jap.c12_s4_pc1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class C12S4Pc1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
